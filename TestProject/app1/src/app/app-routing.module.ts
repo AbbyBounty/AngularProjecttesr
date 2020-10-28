ccad927f841ac854a21c4fb25271456f882c62d3
Devil983 <62641503+Devil983@users.noreply.github.com>
2020-10-28 17:04:13 +0530

    update

--- TestProject/app1/src/app/app-routing.module.ts
+++ TestProject/app1/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { OrderHistoryComponent } from './order-history/order-history.component';
 import { AvailableServicesComponent } from './available-services/available-services.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { VendorregistraionComponent } from './vendorregistraion/vendorregistraion.component';
 
 const routes: Routes = [
  {path:'order-history',component:OrderHistoryComponent},
  {path:'services',component:AvailableServicesComponent},
- {path:'dashboard',component:DashboardComponent}
+ {path:'dashboard',component:DashboardComponent},
+ {path:'vendorregister',component:VendorregistraionComponent}
 ];
 
 @NgModule({
