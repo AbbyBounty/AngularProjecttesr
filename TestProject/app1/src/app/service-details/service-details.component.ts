7745be77775243c2d82c6f9b18c54d3c5981dbb0
Tukaram Pandhare <patil2033.tp@gmail.com>
2020-11-02 06:05:03 -0800

    service details

--- TestProject/app1/src/app/service-details/service-details.component.ts
+++ TestProject/app1/src/app/service-details/service-details.component.ts
@@ -4,22 +4,62 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-service-details',
   templateUrl: './service-details.component.html',
-  styleUrls: ['./service-details.component.css']
+  styleUrls: ['./service-details.component.css'],
 })
 export class ServiceDetailsComponent implements OnInit {
-  serviceName = "";
-  CustomerName = "Tuka";
-  Date = "";
-  VehicleNumber = "";
-  ServiceName = "";
-
-  constructor(private activatedRoute: ActivatedRoute) { }
+  CustomerName = 'Tuka';
+  Date = '';
+  VehicleNumber = '';
+  Amount = '';
+  OrderDate = ' ';
+  Description = '';
+  serviceName = '';
+  ServiceId = '';
+  activeOrders = [
+    {
+      'ServiceId': 161,
+      'ServiceName': 'Full body service',
+      'Description': 'xxxxxxxxxxxxxx',
+      'Amount': 2399
+    },
+    {
+      'ServiceId': 112,
+      'ServiceName': 'Oil Change',
+      'Description': 'xxxxxxxxxxxxxx',
+      'Amount': 2399
+    },
+    {
+      'ServiceId': 3313,
+      'ServiceName': 'Air',
+      'Description': 'xxxxxxxxxxxxxx',
+      'Amount': 2399
+    },
+    {
+      'ServiceId': 124,
+      'ServiceName': 'Washing',
+      'Description': 'xxxxxxxxxxxxxx',
+      'Amount': 2399
+    },
+    {
+      'ServiceId': 155,
+      'ServiceName': 'Full body service',
+      'Description': 'xxxxxxxxxxxxxx',
+      'Amount': 2399
+    },
+    {
+      'ServiceId': 111,
+      'ServiceName': 'Oil Change',
+      'Description': 'xxxxxxxxxxxxxx',
+      'Amount': 2399
+    },
+  ];
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     const num = this.activatedRoute.snapshot.queryParams['num'];
-    alert("In Service Details"+num);
-    if(num){
-      console.log(num)
+    alert('In Service Details' + num);
+    if (num) {
+      console.log(num);
     }
   }
 }
